2e1595c8177cc3db69d0fe20c957ba6f2d19e8f0
KauanDK <lidiane6734@gmail.com>
2021-04-28 12:35:57 -0300

    First commit

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* : LUCI BOT
+──────────────────
+        『 *VIP USER* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*VOCE E UM MEMBRO PREMIUM* 🐊🚩`
+}
+exports.cekvip = cekvip
---
+++ src/daftarvip.js
@@ -0,0 +1,8 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+DA UMA MAMADA NO LUCI 😳😳 `
+}
+exports.daftarvip = daftarvip
